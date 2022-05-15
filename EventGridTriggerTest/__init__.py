31d2ffb7277dda46e4e92898b7f9af00f6397ccc
Hoangvu <hoangvu37>
2022-05-15 13:37:48 +0700

    update

--- EventGridTriggerTest/__init__.py
+++ EventGridTriggerTest/__init__.py
@@ -1,16 +1,17 @@
 import json
 import logging
-
+from bson.json_util import dumps
 import azure.functions as func
 
 
 def main(event: func.EventGridEvent):
-    result = json.dumps({
+    data = {
         'id': event.id,
         'data': event.get_json(),
         'topic': event.topic,
         'subject': event.subject,
         'event_type': event.event_type,
-    })
+    }
+    result = dumps(data)
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
